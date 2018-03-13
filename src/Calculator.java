7692f0e37f437f8139980a74a0cc1d1a24382c64
bayu <bayuteguhpamuji@gmail.com>
2018-03-13 11:03:14 +0700

    [Bayu] finishing Calculator program

---
+++ src/Calculator.java
@@ -0,0 +1,73 @@
+import model.CalculatorModel;
+
+import java.util.Scanner;
+
+public class Calculator {
+    CalculatorModel modelData = new CalculatorModel();
+    public void printCalculator(){
+        Scanner scanner = new Scanner(System.in);
+        try{
+            System.out.print("Nilai 1 : ");
+            modelData.setNum1(scanner.nextInt());
+            System.out.print("Nilai 2 : ");
+            modelData.setNum2(scanner.nextInt());
+            System.out.println("Pilih Operator :");
+            System.out.println("1. Penambahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Pembagian");
+            System.out.print("Pilihan :");
+            int choice = scanner.nextInt();
+            int num1 = modelData.getNum1();
+            int num2 = modelData.getNum2();
+            hitung(num1, num2, choice);
+        }catch (Exception ex){
+            System.out.println("Error, Input data harus bilangan bulat ");
+            printCalculator();
+        }
+    }
+
+    private void hitung(int num1, int num2, int choice){
+        switch (choice){
+            case 1:
+                System.out.println("addition");
+                additionOperator(num1, num2);
+                break;
+            case 2:
+                System.out.println("subtraction");
+                subractionOperator(num1, num2);
+                break;
+            case 3:
+                System.out.println("Multiplication");
+                multiplicationOperator(num1, num2);
+                break;
+            case 4:
+                System.out.println("Division");
+                divisionOperator(num1, num2);
+                break;
+            default:
+                System.out.println("Operator tidak ditemukan");
+        }
+    }
+
+    private void additionOperator(int num1, int num2){
+        int hasil = num1 + num2;
+        System.out.println(num1 +" + "+num2+" = "+hasil);
+    }
+
+    private void subractionOperator(int num1, int num2){
+        int hasil = num1 - num2;
+        System.out.println(num1 +" - "+num2+" = "+hasil);
+    }
+
+    private void multiplicationOperator(int num1, int num2){
+        int hasil = num1 * num2;
+        System.out.println(num1 +" x "+num2+" = "+hasil);
+    }
+
+    private void divisionOperator(int num1, int num2){
+        double hasil = (double) num1 / num2;
+        System.out.println(num1 +" / "+num2+" = "+hasil);
+    }
+
+}
