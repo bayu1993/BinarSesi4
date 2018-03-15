46f15111c979af3a5b92e5602e3bffeec3a6b73b
bayu <bayuteguhpamuji@gmail.com>
2018-03-15 10:15:19 +0700

    [Bayu] perubahan di LiftApps.java

--- src/LiftApps/LiftApps.java
+++ src/LiftApps/LiftApps.java
@@ -6,19 +6,16 @@ public class LiftApps  {
     public LiftApps(){
         Scanner scanner = new Scanner(System.in);
         LiftImpl lift = new LiftImpl();
-        int current = 0;
-        int goal = 0;
         try {
             System.out.print("Posisi anda ada di lantai berapa : ");
-            current = scanner.nextInt();
+            int current = scanner.nextInt();
             lift.cekCurrentPosition(current);
             System.out.print("Tujuan anda ke lantai berapa : ");
-            goal = scanner.nextInt();
+            int goal = scanner.nextInt();
             lift.cekGoalPosition(goal);
+            lift.cekPosition(current,goal);
        }catch (Exception e){
            lift.errorHandling("number");
-       }finally {
-            lift.cekPosition(current,goal);
-        }
+       }
     }
 }
